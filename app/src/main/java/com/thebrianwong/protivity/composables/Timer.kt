b577422cf47aae1ec1de13cd512a4ab9705199de
Brian Wong <brianw7218@gmail.com>
2023-12-08 21:30:35 -0800

    Format time to have leading 0's

--- app/src/main/java/com/thebrianwong/protivity/composables/Timer.kt
+++ app/src/main/java/com/thebrianwong/protivity/composables/Timer.kt
@@ -8,29 +8,33 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import kotlin.math.min
 
 @Composable
 fun Timer(duration: Long) {
     var remainingTime by remember { mutableLongStateOf(duration) }
     val hours = (remainingTime / 3600).toInt()
-    val minutes = (remainingTime - (hours * 3600)) / 60
+    val minutes = ((remainingTime - (hours * 3600)) / 60).toInt()
     val seconds = (remainingTime - (hours * 3600) - (minutes * 60)).toInt()
 
-    val timer = object:CountDownTimer(duration, 1000){
+    val timer = object : CountDownTimer(duration, 1000) {
         override fun onTick(newTime: Long) {
             remainingTime = newTime / 1000
         }
+
         override fun onFinish() {
 //            figure out how to send phone alerts
         }
     }
 
-    DisposableEffect(key1 = duration){
+    fun formatTime(timeUnit: Int) = if (timeUnit < 10) "0$timeUnit" else timeUnit
+
+    DisposableEffect(key1 = duration) {
         timer.start()
         onDispose {
             timer.cancel()
         }
     }
 
-    Text(text = "Remaining time: $hours:$minutes:$seconds")
+    Text(text = "Remaining time: ${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}")
 }
