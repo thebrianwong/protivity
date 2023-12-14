354cae618c3e0515fde7d69ca54c39a042323121
Brian Wong <brianw7218@gmail.com>
2023-12-13 22:30:38 -0800

    Add reset button, adjust spacing, and add ChatGPT placeholder

--- app/src/main/java/com/thebrianwong/protivity/composables/Timer.kt
+++ app/src/main/java/com/thebrianwong/protivity/composables/Timer.kt
@@ -1,12 +1,19 @@
 package com.thebrianwong.protivity.composables
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -16,6 +23,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.thebrianwong.protivity.classes.ProtivityCountDownTimer
@@ -98,4 +108,22 @@ fun Timer(startingDuration: Long) {
     }
     Spacer(modifier = Modifier.size(16.dp))
     TimeIncrementButtons(handleClick = { increaseTimer(it) })
+    Spacer(modifier = Modifier.size(32.dp))
+    Button(onClick = { resetTimer() }, enabled = !isCounting) {
+        Text(text = "Reset")
+    }
+    Spacer(modifier = Modifier.size(32.dp))
+    Box(modifier = Modifier.clip(RoundedCornerShape(20.dp))) {
+
+        Text(
+            text = "INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE ",
+            modifier = Modifier
+                .verticalScroll(
+                    rememberScrollState()
+                )
+                .background(MaterialTheme.colorScheme.tertiaryContainer)
+                .padding(8.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
