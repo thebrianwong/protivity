d68526c2f1510519af93b0c0f0179c4adcfd7b05
Brian Wong <brianw7218@gmail.com>
2023-12-13 21:49:03 -0800

    Style the contents with a background

--- app/src/main/java/com/thebrianwong/protivity/views/Home.kt
+++ app/src/main/java/com/thebrianwong/protivity/views/Home.kt
@@ -1,10 +1,16 @@
 package com.thebrianwong.protivity.views
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +20,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import com.thebrianwong.protivity.composables.FloatingActionButton
 import com.thebrianwong.protivity.composables.TimeModal
@@ -35,7 +46,10 @@ fun Home() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
-                .padding(horizontal = 64.dp),
+                .padding(horizontal = 48.dp, vertical = 128.dp)
+                .shadow(elevation = 8.dp, shape = RoundedCornerShape(20.dp))
+                .background(MaterialTheme.colorScheme.secondaryContainer)
+                .padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
