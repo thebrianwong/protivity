9ef35595cead0a41dfb74e3abfe7f09e8f58b064
Brian Wong <brianw7218@gmail.com>
2023-12-14 22:00:57 -0800

    Prevent the timer from starting if the time is 0

--- app/src/main/java/com/thebrianwong/protivity/composables/Timer.kt
+++ app/src/main/java/com/thebrianwong/protivity/composables/Timer.kt
@@ -1,20 +1,8 @@
 package com.thebrianwong.protivity.composables
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -24,9 +12,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.thebrianwong.protivity.classes.ProtivityCountDownTimer
@@ -103,11 +88,19 @@ fun Timer(startingDuration: Long) {
         text = "${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}",
         fontSize = 64.sp
     )
-    Button(onClick = { handleButtonClick() }, modifier = Modifier.animateContentSize()) {
+    Button(
+        onClick = { handleButtonClick() },
+        modifier = Modifier.animateContentSize(),
+        enabled = startingDuration != 0L || remainingTime != 0L
+    ) {
         Text(text = if (isCounting) "Pause" else if (isNewCounter) "Start" else "Resume")
     }
     TimeIncrementButtons(handleClick = { increaseTimer(it) })
-    Button(onClick = { resetTimer() }, enabled = !isCounting, modifier = Modifier.padding(top = 16.dp)) {
+    Button(
+        onClick = { resetTimer() },
+        enabled = !isCounting,
+        modifier = Modifier.padding(top = 16.dp)
+    ) {
         Text(text = "Reset")
     }
 }
