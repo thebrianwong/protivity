16376e1ec997ef279ff846c665ff60dfc18491d6
Brian Wong <brianw7218@gmail.com>
2024-01-07 21:34:16 -0800

    Make the notification have a proper, looping alarm sound
    Past attempts at this failed because the change is made on the notification channel, not the notification itself. Additionally, changes to the notification channel would not reflect upon uninstalling and reinstalling the app.

--- app/src/main/java/com/thebrianwong/protivity/classes/NotificationUtils.kt
+++ app/src/main/java/com/thebrianwong/protivity/classes/NotificationUtils.kt
@@ -6,6 +6,9 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -14,35 +17,48 @@ import com.thebrianwong.protivity.MainActivity
 import com.thebrianwong.protivity.R
 
 class NotificationUtils(private val context: Context) {
-    private val intent = Intent(context, MainActivity::class.java).apply {
-        flags = Intent.FLAG_ACTIVITY_NEW_TASK and Intent.FLAG_ACTIVITY_CLEAR_TASK
-    }
-    private val pendingIntent: PendingIntent =
-        PendingIntent.getActivity(context, 10, intent, PendingIntent.FLAG_MUTABLE)
-    private val notificationBuilder = NotificationCompat.Builder(context, "protivityChannel")
-        .setSmallIcon(R.drawable.baseline_timer_24)
-        .setContentTitle("Protivity")
-        .setContentText("Time's Up!")
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
-        .setAutoCancel(true)
-        .setDefaults(Notification.DEFAULT_VIBRATE)
-        .setVibrate(longArrayOf(1000, 1000, 1000))
-        .setFullScreenIntent(pendingIntent, true)
-    private val notificationChannel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        NotificationChannel(
-            "protivityChannel",
-            "Protivity Timer",
-            NotificationManager.IMPORTANCE_HIGH
-        ).apply { description = "The notification for when the Protivity timer is up." }
-    } else {
-        null
-    }
-
     fun dispatchNotification() {
+        val intent = Intent(context, MainActivity::class.java)
+            .apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK and Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(context, 10, intent, PendingIntent.FLAG_MUTABLE)
+        val notificationBuilder = NotificationCompat.Builder(context, "protivityChannel")
+            .setSmallIcon(R.drawable.baseline_timer_24)
+            .setContentTitle("Protivity")
+            .setContentText("Time's Up!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setDefaults(Notification.DEFAULT_VIBRATE)
+            .setVibrate(longArrayOf(1000, 1000, 1000))
+            .setFullScreenIntent(pendingIntent, true)
+
+        val alarmSound: Uri? = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        val alarmAudioAttributes =
+            AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_ALARM).build()
+
+        val notificationChannel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel(
+                "protivityChannel",
+                "Protivity Timer",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "The notification for when the Protivity timer is up."
+                vibrationPattern = longArrayOf(1000, 1000, 1000)
+                setSound(
+                    alarmSound,
+                    alarmAudioAttributes
+                )
+                enableVibration(true)
+            }
+        } else {
+            null
+        }
+
         val notificationManager: NotificationManager =
             getSystemService(context, NotificationManager::class.java) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && notificationChannel != null) {
-            notificationChannel.enableVibration(true)
             notificationManager.createNotificationChannel(notificationChannel)
         }
         val notification = notificationBuilder.build()
