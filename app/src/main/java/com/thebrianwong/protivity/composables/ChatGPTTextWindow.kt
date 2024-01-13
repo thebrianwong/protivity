91914839173d5fa849edb7c46ba60b4a29b0d335
Brian Wong <brianw7218@gmail.com>
2024-01-12 22:39:08 -0800

    Adjust the styling of the ChatGPT window to always occupy the same space

--- app/src/main/java/com/thebrianwong/protivity/composables/ChatGPTTextWindow.kt
+++ app/src/main/java/com/thebrianwong/protivity/composables/ChatGPTTextWindow.kt
@@ -2,6 +2,7 @@ package com.thebrianwong.protivity.composables
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,23 +10,32 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.thebrianwong.protivity.viewModels.ChatGPTViewModel
 
 @Composable
-fun ChatGPTTextWindow() {
-    Box(modifier = Modifier.clip(RoundedCornerShape(20.dp))) {
+fun ChatGPTTextWindow(viewModel: ChatGPTViewModel) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(20.dp))
+            .background(MaterialTheme.colorScheme.tertiaryContainer)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
         Text(
-            text = "INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE INSERT CHATGPT HERE ",
+            text = viewModel.generatedText.value,
             modifier = Modifier
                 .verticalScroll(
                     rememberScrollState()
                 )
-                .background(MaterialTheme.colorScheme.tertiaryContainer)
+                .clip(RoundedCornerShape(20.dp))
+                .fillMaxSize()
                 .padding(8.dp),
-            textAlign = TextAlign.Justify
+            textAlign = TextAlign.Center
         )
     }
 }
